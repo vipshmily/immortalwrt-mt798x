5c4d9a959e1ba8ad8d4c0c98ce1470e19c5e83b0
vipshmily <85991289+vipshmily@users.noreply.github.com>
2024-12-17 16:50:44 +0800

    update 21_ethinfo.js

--- package/emortal/autocore/files/generic/21_ethinfo.js
+++ package/emortal/autocore/files/generic/21_ethinfo.js
@@ -1,10 +1,25 @@
 'use strict';
 'require baseclass';
 'require rpc';
+'require network';
 
-var callLuciETHInfo = rpc.declare({
+var callSwconfigFeatures = rpc.declare({
   object: 'luci',
-  method: 'getETHInfo',
+  method: 'getSwconfigFeatures',
+  params: ['switch'],
+  expect: { '': {} }
+});
+
+var callSwconfigPortState = rpc.declare({
+  object: 'luci',
+  method: 'getSwconfigPortState',
+  params: ['switch'],
+  expect: { result: [] }
+});
+
+var callLuciBoardJSON = rpc.declare({
+  object: 'luci-rpc',
+  method: 'getBoardJSON',
   expect: { '': {} }
 });
 
@@ -14,80 +29,151 @@ var callLuciNetworkDevices = rpc.declare({
   expect: { '': {} }
 });
 
+var isDSA = false;
+
+const ethStyle = {
+  box: 'max-width: 100px;',
+  head: `
+    border-radius: 7px 7px 0 0;
+    text-align: center;
+    font-weight: bold;`,
+  body: `
+    border: 1px solid lightgrey;
+    border-radius: 0 0 7px 7px;
+    display: flex; flex-direction: column;
+    align-items: center; justify-content: center;`,
+  icon: 'margin: 5px; width: 40px;',
+  speed: 'font-size: 0.8rem; font-weight: bold;',
+  traffic: `
+    border-top: 1px solid lightgrey;
+    font-size: 0.8rem;`
+};
+
 function formatSpeed(speed) {
-  if (speed == '-') return '-';
-  const speedNum = parseInt(speed);
-  return speedNum < 1000 ? `${speedNum} M` : `${speedNum / 1000} GbE`;
+  if (speed <= 0) return '-';
+  const speedInt = parseInt(speed);
+  if (isNaN(speedInt)) return '-';
+  return speedInt < 1000 ? `${speedInt} M` : `${speedInt / 1000} GbE`;
 }
 
-function getPortColor(link, duplex) {
-  if (link == 'no') return 'background-color: whitesmoke;';
-  const color = duplex == 'Full' ? 'greenyellow' : 'darkorange';
-  return 'background-color: ' + color;
+function getPortColor(carrier, duplex) {
+  if (!carrier) return 'background-color: whitesmoke;';
+  if (duplex === 'full' || duplex === true)
+    return 'background-color: greenyellow;';
+  return 'background-color: darkorange';
 }
 
-function getPortIcon(link) {
-  return L.resource(`icons/port_${link == 'yes' ? 'up' : 'down'}.png`);
+function getPortIcon(carrier) {
+  return L.resource(`icons/port_${carrier ? 'up' : 'down'}.png`);
 }
 
-return L.Class.extend({
+function portDom(link, duplex, label, speed, tx_bytes, rx_bytes) {
+  const portIcon = getPortIcon(link);
+  const portColor = getPortColor(link, duplex);
+
+  return E('div', { style: ethStyle.box }, [
+    E('div', { style: ethStyle.head + portColor }, label),
+    E('div', { style: ethStyle.body }, [
+      E('img', { style: ethStyle.icon, src: portIcon }),
+      E('div', { style: ethStyle.speed }, formatSpeed(speed)),
+      E('div', { style: ethStyle.traffic }, [
+        '\u25b2\u202f%1024.1mB'.format(tx_bytes),
+        E('br'),
+        '\u25bc\u202f%1024.1mB'.format(rx_bytes)
+      ])
+    ])
+  ]);
+}
+
+return baseclass.extend({
   title: _('Ethernet Information'),
 
   load: function () {
-    return Promise.all([
-      L.resolveDefault(callLuciETHInfo(), {}),
-      L.resolveDefault(callLuciNetworkDevices(), {})
-    ]);
+    return network.getSwitchTopologies().then(function (topologies) {
+      if (Object.keys(topologies).length === 0) {
+        isDSA = true;
+        return Promise.all([
+          L.resolveDefault(callLuciBoardJSON(), {}),
+          L.resolveDefault(callLuciNetworkDevices(), {})
+        ]);
+      }
+
+      callSwconfigPortState('switch0').then((ports) => {
+        topologies.switch0.portstate = ports;
+      });
+      return Promise.all([
+        topologies,
+        L.resolveDefault(callLuciBoardJSON(), {}),
+        L.resolveDefault(callLuciNetworkDevices(), {})
+      ]);
+    });
   },
 
-  render: function (data) {
-    const ethinfo = Array.isArray(data[0].ethinfo) ? data[0].ethinfo : [];
-    const netdevs = typeof data[1] === 'object' ? data[1] : {};
+  render_gsw: function (data) {
+    const topologies = data[0];
+    const board = data[1];
+    const netdevs = data[2];
 
-    const boxStyle = 'max-width: 100px;';
-    const boxHeadStyle =
-      'border-radius: 7px 7px 0 0;' +
-      'text-align: center;' +
-      'font-size:1.1rem; font-weight:bold;';
-    const boxbodyStyle =
-      'border: 1px solid lightgrey;' +
-      'border-radius: 0 0 7px 7px;' +
-      'display:flex; flex-direction: column;' +
-      'align-items: center; justify-content:center;';
-    const iconStyle = 'margin: 5px; width: 40px;';
-    const speedStyle = 'font-size:0.8rem; font-weight:bold;';
-    const trafficStyle =
-      'border-top: 1px solid lightgrey;' + 'font-size:0.8rem;';
-
+    let stats;
+    let foundWAN = false;
     const ethPorts = [];
-    for (const port of ethinfo) {
-      const portName = port.name;
-      const portIcon = getPortIcon(port.status);
-      const portColor = getPortColor(port.status, port.duplex);
-      const { tx_bytes, rx_bytes } = netdevs[portName].stats;
-      const portDiv = E('div', { style: boxStyle }, [
-        E('div', { style: boxHeadStyle + portColor }, portName),
-        E('div', { style: boxbodyStyle }, [
-          E('img', { style: iconStyle, src: portIcon }),
-          E('div', { style: speedStyle }, formatSpeed(port.speed)),
-          E('div', { style: trafficStyle }, [
-            '\u25b2\u202f%1024.1mB'.format(tx_bytes),
-            E('br'),
-            '\u25bc\u202f%1024.1mB'.format(rx_bytes)
-          ])
-        ])
-      ]);
-      if (portName == 'wan') {
-        ethPorts.unshift(portDiv);
-      } else {
-        ethPorts.push(portDiv);
+    const switch0 = topologies.switch0;
+    for (const port of switch0.ports) {
+      const label = port.label.toUpperCase();
+      const { link, duplex, speed } = switch0.portstate[port.num];
+      const txrx = { tx_bytes: 0, rx_bytes: 0 };
+
+      if (label.startsWith('WAN')) {
+        foundWAN = true;
+        stats = netdevs[board.network.wan.device].stats;
+        const { tx_bytes, rx_bytes } = stats;
+        ethPorts.unshift(
+          portDom(link, duplex, 'WAN', speed, tx_bytes, rx_bytes)
+        );
+      } else if (label.startsWith('LAN')) {
+        stats = netdevs['br-lan'].stats;
+        const { tx_bytes, rx_bytes } = link ? stats : txrx;
+        ethPorts.push(portDom(link, duplex, label, speed, tx_bytes, rx_bytes));
       }
     }
 
-    const gridStyle =
-      'display:grid; grid-gap: 5px 5px;' +
-      'grid-template-columns:repeat(auto-fit, minmax(70px, 1fr));' +
-      'margin-bottom:1em';
+    if (foundWAN) return ethPorts;
+
+    const wan = netdevs[board.network.wan.device];
+    const { speed, duplex, carrier } = wan.link;
+    const { tx_bytes, rx_bytes } = wan.stats;
+    ethPorts.unshift(
+      portDom(carrier, duplex, 'WAN', speed, tx_bytes, rx_bytes)
+    );
+    return ethPorts;
+  },
+
+  render_dsa: function (data) {
+    const board = data[0];
+    const netdevs = data[1];
+
+    const ethPorts = [];
+    const wan = board.network.wan.device;
+    let devices = `${wan},lan0,lan1,lan2,lan3,lan4,lan5,lan6`;
+    devices = devices.split(',');
+    for (const device of devices) {
+      if (device in netdevs === false) continue;
+      const dev = netdevs[device];
+      const label = dev.name;
+      const { speed, duplex, carrier } = dev.link;
+      const { tx_bytes, rx_bytes } = dev.stats;
+      ethPorts.push(portDom(carrier, duplex, label, speed, tx_bytes, rx_bytes));
+    }
+
+    return ethPorts;
+  },
+
+  render: function (data) {
+    const ethPorts = isDSA ? this.render_dsa(data) : this.render_gsw(data);
+    const gridStyle = `
+      display: grid; grid-gap: 5px 5px;
+      grid-template-columns: repeat(auto-fit, minmax(70px, 1fr));
+      margin-bottom: 1em`;
     return E('div', { style: gridStyle }, ethPorts);
   }
 });
